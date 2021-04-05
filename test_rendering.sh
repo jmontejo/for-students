4c4ed4111108c447cd9d1db41761f26e0b232ce7
Chiara Rizzi <chiara.rizzi@cern.ch>
2021-04-05 11:15:02 +0200

    test rendering

---
+++ test_rendering.sh
@@ -0,0 +1,8 @@
+git clone https://gitlab.cern.ch/crizzi/jekyll-theme-chiara.git jekyll-theme-chiara
+cd jekyll-theme-chiara 
+rm -rf .git* .travis.yml README.md LICENSE.txt 
+cd ../
+mv -vn jekyll-theme-chiara/* .
+rm -rf jekyll-theme-chiara
+gem install bundler
+bundle exec jekyll serve --trace
