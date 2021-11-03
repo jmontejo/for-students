b82c678082d7259ace7bccc48be59ecf583398d4
Javier Montejo <jmontejo@cern.ch>
2021-11-03 23:13:56 +0100

    My theme

--- test_rendering.sh
+++ test_rendering.sh
@@ -1,9 +1,9 @@
-git clone https://gitlab.cern.ch/crizzi/jekyll-theme-chiara.git jekyll-theme-chiara
-cd jekyll-theme-chiara 
+git clone https://gitlab.cern.ch/jmontejo/jekyll-theme-jmontejo.git jekyll-theme-jmontejo
+cd jekyll-theme-jmontejo
 rm -rf .git* .travis.yml README.md LICENSE.txt 
 cd ../
-mv -vn jekyll-theme-chiara/* .
-rm -rf jekyll-theme-chiara
+mv -vn jekyll-theme-jmontejo/* .
+rm -rf jekyll-theme-jmontejo
 gem install bundler
 bundle install
 bundle exec jekyll serve --trace
